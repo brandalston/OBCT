7184b3e77e960e997f05ecb2b2280b5f87f85f7f
brandonalston <bca3@rice.edu>
2022-03-07 09:04:42 -0600

    Code

--- Code/TEST_CODE.py
+++ Code/TEST_CODE.py
@@ -1,25 +1,26 @@
-
 import model_runs
 import pareto_runs
 
 
-### MODEL RUN EXAMPLE
+# MODEL RUN EXAMPLE
 data_names = ['monk1_enc']
 heights = [2]
-models = ['FlowOCT','BendersOCT','MCF1','MCF2','CUT1','CUT2']
+models = ['FlowOCT', 'BendersOCT', 'MCF1', 'MCF2', 'CUT1', 'CUT2']
 time_limit = 3600
-extras = ['fixing','max_features-15']
+extras = ['fixing', 'max_features-15']
 rand_states = [138]
 tuning = None
 file = 'results.csv'
 plot_fig = False
 consol_log = True
-model_runs.main(["-d",data_names,"-h",heights,"-m",models,"-t",time_limit,"-e",extras,"-r",rand_states,"-c", tuning,"-f",file,"-p",plot_fig,"-l", consol_log])
+model_runs.main(
+    ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit, "-e", extras, "-r", rand_states, "-c", tuning,
+     "-f", file, "-p", plot_fig, "-l", consol_log])
 
-### PARETO FRONTIER EXAMPLE
+# PARETO FRONTIER EXAMPLE
 height = 5
-models = ['AGHA','MCF1','MCF2','CUT1','CUT2']
+models = ['AGHA', 'MCF1', 'MCF2', 'CUT1', 'CUT2']
 repeats = 5
-data_names = ['soybean-small_enc','monk1_enc']
+data_names = ['soybean-small_enc', 'monk1_enc']
 file = 'pareto.csv'
-pareto_runs.main(["-d",data_names,"-h",height,"-m",models,"-t",3600,"-r",repeats,"-f",file])
+pareto_runs.main(["-d", data_names, "-h", height, "-m", models, "-t", 3600, "-r", repeats, "-f", file])
