071f7d9f59637ee45ea2db33d3141adb2ca39df8
brandonalston <bca3@rice.edu>
2022-03-07 09:04:13 -0600

    Code

--- Code/TEST_CODE.py
+++ Code/TEST_CODE.py
@@ -3,6 +3,7 @@ import model_runs
 import pareto_runs
 
 
+### MODEL RUN EXAMPLE
 data_names = ['monk1_enc']
 heights = [2]
 models = ['FlowOCT','BendersOCT','MCF1','MCF2','CUT1','CUT2']
@@ -15,11 +16,10 @@ plot_fig = False
 consol_log = True
 model_runs.main(["-d",data_names,"-h",heights,"-m",models,"-t",time_limit,"-e",extras,"-r",rand_states,"-c", tuning,"-f",file,"-p",plot_fig,"-l", consol_log])
 
-'''
+### PARETO FRONTIER EXAMPLE
 height = 5
 models = ['AGHA','MCF1','MCF2','CUT1','CUT2']
 repeats = 5
 data_names = ['soybean-small_enc','monk1_enc']
 file = 'pareto.csv'
 pareto_runs.main(["-d",data_names,"-h",height,"-m",models,"-t",3600,"-r",repeats,"-f",file])
-'''
