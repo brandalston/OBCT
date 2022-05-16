662361448ca56fccf44b3f9ea638119acb721ec1
brandonalston <bca3@rice.edu>
2022-05-16 13:35:33 -0500

    warm start testing

--- Code/testing_4.py
+++ Code/testing_4.py
@@ -2,24 +2,23 @@ import model_runs
 import pareto_runs
 
 time_limit = 3600
-rand_states = [0]
+rand_states = [42, 0]
 file = 'warm_start_results.csv'
-heights = [3]
+heights = [4]
 data_names = ['tic-tac-toe_enc']
 models = ['CUT2']
-extras = None
 tuning = 'warm_start'
+extras = None
 model_runs.main(
     ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit, "-e", extras, "-r", rand_states, "-c", tuning,
      "-f", file, "-p", False, "-l", False])
 
 rand_states = [138, 15, 89, 42, 0]
-heights = [4,5]
+heights = [5]
 data_names = ['tic-tac-toe_enc']
 model_runs.main(
     ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit, "-e", extras, "-r", rand_states, "-c", tuning,
      "-f", file, "-p", False, "-l", False])
-
 
 rand_states = [138, 15, 89, 42, 0]
 heights = [3,4,5]
--- Code/testing_7.py
+++ Code/testing_7.py
@@ -1,19 +1,10 @@
 import model_runs
 import pareto_runs
 
-time_limit = 3600
-rand_states = [42, 0]
-file = 'warm_start_results.csv'
-heights = [4]
-data_names = ['kr-vs-kp_enc']
-models = ['CUT1']
-extras = None
-tuning = 'warm_start'
-model_runs.main(
-    ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit, "-e", extras, "-r", rand_states, "-c", tuning,
-     "-f", file, "-p", False, "-l", False])
 
-rand_states = [138, 15, 89, 42, 0]
+time_limit = 3600
+file = 'warm_start_results.csv'
+rand_states = [89, 42, 0]
 heights = [5]
 data_names = ['kr-vs-kp_enc']
 models = ['CUT1']
