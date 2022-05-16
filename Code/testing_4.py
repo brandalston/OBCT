6513de4cf57152178a20d2e5eb029c94937ccf22
brandonalston <bca3@rice.edu>
2022-05-16 08:16:55 -0500

    warm start testing

--- Code/testing_4.py
+++ Code/testing_4.py
@@ -2,13 +2,28 @@ import model_runs
 import pareto_runs
 
 time_limit = 3600
-rand_states = [138, 15, 89, 42, 0]
+rand_states = [0]
 file = 'warm_start_results.csv'
-heights = [3,4,5]
-data_names = ['tic-tac-toe_enc','car_evaluation_enc']
+heights = [3]
+data_names = ['tic-tac-toe_enc']
 models = ['CUT2']
 extras = None
 tuning = 'warm_start'
+model_runs.main(
+    ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit, "-e", extras, "-r", rand_states, "-c", tuning,
+     "-f", file, "-p", False, "-l", False])
+
+rand_states = [138, 15, 89, 42, 0]
+heights = [4,5]
+data_names = ['tic-tac-toe_enc']
+model_runs.main(
+    ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit, "-e", extras, "-r", rand_states, "-c", tuning,
+     "-f", file, "-p", False, "-l", False])
+
+
+rand_states = [138, 15, 89, 42, 0]
+heights = [3,4,5]
+data_names = ['car_evaluation_enc']
 model_runs.main(
     ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit, "-e", extras, "-r", rand_states, "-c", tuning,
      "-f", file, "-p", False, "-l", False])
