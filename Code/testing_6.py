81a678288ebb0700fcf40f6b7bc4caab1faf90b7
brandonalston <bca3@rice.edu>
2022-05-17 20:22:32 -0500

    warm start testing

--- Code/testing_6.py
+++ Code/testing_6.py
@@ -2,10 +2,10 @@ import model_runs
 import pareto_runs
 
 time_limit = 3600
-rand_states = [138, 15, 89, 42, 0]
+rand_states = [0]
 file = 'warm_start_results.csv'
-heights = [3,4,5]
-data_names = ['kr-vs-kp_enc','fico_binary_enc']
+heights = [4]
+data_names = ['fico_binary_enc']
 models = ['MCF2']
 extras = None
 tuning = 'warm_start'
