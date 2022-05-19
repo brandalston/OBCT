8b0b63bbf3e5506c1711ccf2748ac52220171881
brandonalston <bca3@rice.edu>
2022-05-19 06:42:48 -0500

    Pareto Frontiers

--- Code/pareto_runs.py
+++ Code/pareto_runs.py
@@ -5,16 +5,11 @@ import pandas as pd
 import getopt
 import sys
 import csv
-from os.path import exists
 from sklearn.model_selection import train_test_split
 from OBCT import OBCT
 from TREE import TREE
 import UTILS as OU
-import RESULTS as OR
-from FlowOCT import FlowOCT
-from BendersOCT import BendersOCT
-import FlowOCTutils
-from FlowOCTTree import Tree as FlowOCTTree
+
 
 def main(argv):
     print(argv)
@@ -46,12 +41,13 @@ def main(argv):
             file_out = arg
 
     ''' Columns of the results file generated '''
-    summary_columns = ['Data', 'H', '|I|', 'Out-Acc', 'In-Acc', 'Sol-Time', 'Gap', 'ObjVal',
-                       '# CB', 'User Cuts', 'Cuts/CB', 'CB-Time', 'INT-CB-Time', 'FRAC-CB-Time', 'CB-Eps',
-                       'Model', 'Time Limit', 'Rand. State',
-                       '% Fixed', 'Calibration', 'CC',
-                       'Single Feature Use', 'Level Tree', 'Max Features', 'Super Feature']
-    output_path = os.getcwd() + '/results_files/'
+    summary_columns = ['Data', 'H', '|I|',
+                       'Out_Acc', 'In_Acc', 'Sol_Time',
+                       'MIP_Gap', 'Obj_Val', 'Obj_Bound', 'Model',
+                       'Num_CB', 'User_Cuts', 'Cuts_per_CB',
+                       'Total_CB_Time', 'INT_CB_Time', 'FRAC_CB_Time', 'CB_Eps',
+                       'Time_Limit', 'Rand_State', 'Calibration', 'Single_Feature_Use', 'Max_Features']
+    output_path = os.getcwd() + '/Code/results_files/'
     if file_out is None:
         output_name = str(data_files) + '_H:' + str(height) + '_' + str(modeltypes) + \
                       '_T:' + str(time_limit) + '_pareto.csv'
@@ -71,7 +67,7 @@ def main(argv):
 
     for file in data_files:
         # pull dataset to train model with
-        data, encoding_map = OU.get_data(file.replace('.csv', ''), target)
+        data = OU.get_data(file.replace('.csv', ''))
         for i in range(repeats):
             print('\n\nDataset: ' + str(file) + ', H: ' + str(height) + ', Iteration: ' + str(i) + '. Run Start: ' + str(
                   time.strftime("%I:%M %p", time.localtime())))
@@ -82,43 +78,39 @@ def main(argv):
                     extras = [f'num_features-{num_features}']
                     tree = TREE(h=height)
                     opt_model = OBCT(data=train_set, tree=tree, target=target, model=modeltype,
-                                     time_limit=time_limit, encoding_map=encoding_map, model_extras=extras,
-                                     unreachable=None, warm_start=WSV, name=file)
+                                     time_limit=time_limit, model_extras=extras, warm_start=WSV, name=file)
                     opt_model.formulation()
                     opt_model.extras()
                     opt_model.model.update()
                     opt_model.optimization()
-                    OR.node_assign(opt_model, tree)
-                    OR.tree_check(tree)
-                    OR.model_summary(opt_model=opt_model, tree=tree, test_set=test_set,
-                                     rand_state=rand_states[i], results_file=out_file, fig_file=None)
-                    model_wsm_acc, model_wsm_assgn = OR.model_acc(tree=tree, target=target,
+                    OU.node_assign(opt_model, tree)
+                    OU.tree_check(tree)
+                    OU.model_summary(opt_model=opt_model, tree=tree, test_set=test_set,
+                                     rand_state=rand_states[i], results_file=out_file)
+                    model_wsm_acc, model_wsm_assgn = OU.model_acc(tree=tree, target=target,
                                                                   data=train_set)
-                    WSV = {'tree': tree.DG_prime.nodes(data=True), 'data': model_wsm_assgn}
-                    if 'AGHA' == modeltype: WSV = {'tree': tree.DG_prime.nodes(data=True), 'data': False}
+                    WSV = {'tree': tree, 'data': model_wsm_assgn}
+                    if 'AGHA' == modeltype: WSV = {'tree': tree, 'data': False}
 
         # Generate pareto plot of models using run averages of pareto.csv file
-        pareto_data = pd.pareto_data = pd.read_csv(f'results_files/{file_out}', na_values='?')
+        pareto_data = pd.pareto_data = pd.read_csv(os.getcwd()+'/Code/results_files/'+file_out, na_values='?')
         file_data = pareto_data[pareto_data['Data'] == file.replace('.csv', '')]
         frontier_avg = pd.DataFrame(columns=summary_columns)
         for model in ['AGHA','MCF1','MCF2','CUT1','CUT2','FlowOCT','BendersOCT']:
             sub_data = file_data.loc[file_data['Model'] == model]
-            for feature in sub_data['Max Features'].unique():
-                subsub_data = sub_data.loc[sub_data['Max Features'] == feature]
+            for feature in sub_data['Max_Features'].unique():
+                subsub_data = sub_data.loc[sub_data['Max_Features'] == feature]
                 frontier_avg = frontier_avg.append({
                     'Data': file.replace('.csv', ''), 'H': int(subsub_data['H'].mean()),
                     '|I|': int(subsub_data['|I|'].mean()),
-                    'Out-Acc': 100 * subsub_data['Out-Acc'].mean(), 'In-Acc': 100 * subsub_data['In-Acc'].mean(),
-                    'Sol-Time': subsub_data['Sol-Time'].mean(),
-                    'Gap': 100 * subsub_data['Gap'].mean(), 'ObjVal': subsub_data['ObjVal'].mean(),
-                    '# CB': subsub_data['# CB'].mean(), 'User Cuts': subsub_data['User Cuts'].mean(),
-                    'Cuts/CB': subsub_data['Cuts/CB'].mean(),
-                    'CB-Time': subsub_data['CB-Time'].mean(), 'INT-CB-Time': subsub_data['INT-CB-Time'].mean(),
-                    'FRAC-CB-Time': subsub_data['FRAC-CB-Time'].mean(),
-                    'CB-Eps': subsub_data['CB-Eps'].mean(), 'Model': model, 'Time Limit': time_limit,
-                    'Rand State': 'None',
-                    '% Fixed': 0, 'Calibration': False, 'CC': False, 'Single Feature Use': False,
-                    'Level Tree': False,
-                    'Max Features': float(feature), 'Super Feature': False
+                    'Out_Acc': 100 * subsub_data['Out_Acc'].mean(), 'In_Acc': 100 * subsub_data['In_Acc'].mean(),
+                    'Sol_Time': subsub_data['Sol_Time'].mean(), 'MIP_Gap': 100 * subsub_data['MIP_Gap'].mean(),
+                    'Obj_Bound': subsub_data['Obj_Bound'].mean(), 'Obj_Val': 100*subsub_data['Obj_Val'].mean(), 'Model': model,
+                    'Num_CB': subsub_data['Num_CB'].mean(), 'User_Cuts': subsub_data['User_Cuts'].mean(),
+                    'Cuts_per_CB': subsub_data['Cuts_per_CB'].mean(),
+                    'Total_CB_Time': subsub_data['Total_CB_Time'].mean(), 'INT_CB_Time': subsub_data['INT_CB_Time'].mean(),
+                    'FRAC_CB_Time': subsub_data['FRAC_CB_Time'].mean(), 'CB_Eps': subsub_data['CB_Eps'].mean(),
+                    'Time_Limit': time_limit, 'Rand_State': 'None',
+                    'Calibration': False, 'Single_Feature_Use': False, 'Max_Features': float(feature)
                 }, ignore_index=True)
-        OR.pareto_plot(frontier_avg)
+        OU.pareto_plot(frontier_avg)
