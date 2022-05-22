0244991c7254bd64f11496bff929f56e2a82d9f6
brandonalston <bca3@rice.edu>
2022-05-22 13:58:31 -0500

    example code file

--- Code/example_code.py
+++ Code/example_code.py
@@ -1,7 +1,7 @@
 import model_runs
 import pareto_runs
 
-"""
+
 # MODEL RUN EXAMPLE
 data_names = ['soybean-small_enc','monk3_enc','balance-scale_enc','car_evaluation_enc']
 heights = [3,4,5]
@@ -15,7 +15,7 @@ consol_log = False
 model_runs.main(
     ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit,
     "-e", extras, "-r", rand_states, "-c", tuning, "-f", file, "-l", consol_log])
-"""
+
 # PARETO FRONTIER EXAMPLE
 height = 2
 models = ['FOCT', 'MCF1', 'MCF2', 'CUT1', 'CUT2']
