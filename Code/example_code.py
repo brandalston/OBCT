3abfcdc5173193f9c431996072d19e15a882f8af
brandonalston <bca3@rice.edu>
2022-06-08 07:39:30 -0500

    example use of OBCT and pareto frontier

--- Code/example_code.py
+++ Code/example_code.py
@@ -8,18 +8,18 @@ heights = [3,4,5]
 models = ['FlowOCT', 'BendersOCT', 'MCF1', 'MCF2', 'CUT1-ALL', 'CUT2-FRAC1']
 time_limit = 3600
 extras = ['regularization-3']
-rand_states = [138, 15, 89, 42, 0]
+rand_seed = [138, 15, 89, 42, 0]
 tuning = None
 file = 'test_results.csv'
 log_files = False
 model_runs.main(
     ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit,
-    "-e", extras, "-r", rand_states, "-c", tuning, "-f", file, "-l", log_files])
+    "-e", extras, "-r", rand_seed, "-c", tuning, "-f", file, "-l", log_files])
 
 # PARETO FRONTIER EXAMPLE
 height = 2
 models = ['FOCT', 'MCF1', 'MCF2', 'CUT1', 'CUT2']
-rand_states = [15, 78, 0]
+rand_seed = [15, 78, 0]
 data_names = ['hayes-roth_enc', 'house-votes-84_enc']
 file = 'pareto_test.csv'
-pareto_runs.main(["-d", data_names, "-h", height, "-m", models, "-t", 3600, "-r", rand_states, "-f", file])
+pareto_runs.main(["-d", data_names, "-h", height, "-m", models, "-t", 3600, "-r", rand_seed, "-f", file])
