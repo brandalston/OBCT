ffee5a5f67e2bb4e78b6ba2b00d9887b77b3ee01
brandonalston <bca3@rice.edu>
2023-09-14 14:31:10 -0500

    recreate all results found in the paper

--- paper_results.py
+++ paper_results.py
@@ -1,7 +1,6 @@
-import model_runs, pareto_runs
-from Benchmark_Methods import DL8_5, GOSDTg, FB_OCT
+import model_runs
+from Benchmark_Methods import DL85, GOSDTg, FB_OCT
 
-# Paper solution time, gap, and accuracy results
 numerical_datasets = ['iris', 'blood', 'climate', 'ionosphere']
 categorical_datasets = ['balance_scale', 'car', 'kr_vs_kp', 'house_votes_84', 'hayes_roth', 'breast_cancer',
                         'monk1', 'monk2', 'monk3', 'soybean_small', 'spect', 'tic_tac_toe', 'fico_binary']
@@ -14,28 +13,29 @@ extras = None
 rand_states = [138, 15, 89, 42, 0]
 tuning = None
 file = 'paper_results.csv'
-consol_log = False
+log_file = False
 
-model_runs.main(["-d", numerical_datasets + categorical_datasets, "-h", heights, "-m", models, "-t", time_limit,
-                 "-e", extras, "-r", rand_states, "-c", tuning, "-f", file, "-l", consol_log])
-
-model_runs.main(["-d", numerical_datasets + categorical_datasets, "-h", heights, "-m", models, "-t", time_limit,
-                 "-e", extras, "-r", rand_states, "-c", tuning, "-f", file, "-l", consol_log])
+# Accuracy and solution time (gap) results
+model_runs.main(["-d", numerical_datasets + categorical_datasets, "-h", heights, "-m", models,
+                 "-t", time_limit, "-r", rand_states, "-f", file, "-l", False, "-e", None])
 
 FB_OCT.main(["-d", numerical_datasets+categorical_datasets, "-h", heights, "-m", ['Flow', 'Benders'],
-             "-t", time_limit, "-e", extras, "-r", rand_states, "-f", file, "-l", consol_log])
+             "-t", time_limit, "-e", extras, "-r", rand_states, "-f", file, "-l", log_file])
 
-DL8_5.main(["-d", numerical_datasets+categorical_datasets, "-h", heights,
+DL85.main(["-d", numerical_datasets+categorical_datasets, "-h", heights,
             "-t", time_limit, "-r", rand_states, "-f", file])
 
 GOSDTg.main(["-d", numerical_datasets+categorical_datasets, "-h", heights,
              "-t", time_limit, "-r", rand_states, "-f", file])
 
+# Bi-objective results
+models = ['MCF1', 'MCF2', 'CUT1, CUT2']
+priorities = ['data', 'equal']
+model_runs.biobjective(["-d", ['breast_cancer'], "-h", [5], "-m", models, "-t", 3600,
+                     "-p", priorities, "-r", rand_states, "-f", file, "-l", log_file])
 
-# Paper pareto frontier results
-height = 5
-models = ['FOCT', 'MCF1', 'MCF2', 'CUT1', 'CUT2']
-repeats = 5
-data_names = ['soybean_small', 'monk1', 'monk3', 'house-votes-84']
-file = None
-pareto_runs.main(["-d", data_names, "-h", height, "-m", models, "-t", 3600, "-r", repeats, "-f", file])
+# Pareto frontier results
+models = ['FlowOCT', 'MCF1', 'MCF2', 'CUT1', 'CUT2']
+data_names = ['soybean_small', 'monk1', 'monk3', 'house_votes_84']
+model_runs.pareto(["-d", data_names, "-h", 5, "-m", models,
+                   "-t", 3600, "-r", rand_states, "-f", None])
